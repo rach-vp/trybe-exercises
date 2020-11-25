8ca76b5cfed87b15a888e103e0d8ec100f49b9e0
rach-vp <picanco.raquel@gmail.com>
2020-11-25 18:33:24 -0400

    Exclui parte eme xcesso do exercício 7

--- 01_Web_Development_Introduction/04_Javascript_Introduction/4.1_/4.1_Javascript_First_Steps/4.1_JS_First_Steps/7.js
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.1_/4.1_Javascript_First_Steps/4.1_JS_First_Steps/7.js
@@ -15,30 +15,6 @@ O programa deve retornar uma mensagem de erro e encerrar se a nota passada for m
 
 let nota = 50;
 
-// Perguntar no plantão sobre o switch
-switch (nota) {
-    case (nota < 50 && nota >= 0):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito F.');
-        break;
-    case (nota < 60 && nota >= 50):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito E.');
-        break;
-    case (nota < 70 && nota >= 60):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito D.');
-        break;
-    case (nota < 80 && nota >= 70):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito C.');
-        break;
-    case (nota < 90 && nota >= 80):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito B.');
-        break;
-    case (nota < 100 && nota >= 90):
-        console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito A.');
-        break;
-    default:
-        console.log('Nota com valor inválido.');
-}
-
 if (nota < 50 && nota >= 0){
     console.log('De a acordo com a sua nota (' + nota + ' pontos), você é conceito F.');
 }
