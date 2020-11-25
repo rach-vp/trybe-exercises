0ccc91183e8b0b81bbad34b1146cd4b806f11dd8
rach-vp <picanco.raquel@gmail.com>
2020-11-25 16:16:21 -0400

    Resolução do exercício 4

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.1_/4.1_Javascript_First_Steps/4.1_JS_First_Steps/4.js
@@ -0,0 +1,13 @@
+// Retorno se o valor da variável é negativo, positivo ou nulo
+
+a = 0;
+
+if (a > 0) {
+    console.log(a + ' é positivo.');
+}
+else if (a < 0) {
+    console.log(a + ' é negativo.');
+}
+else {
+    console.log(a + ' é igual a 0.');
+}
