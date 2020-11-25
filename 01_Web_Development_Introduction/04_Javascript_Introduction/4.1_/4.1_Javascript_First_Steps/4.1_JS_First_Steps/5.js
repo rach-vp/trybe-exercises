e9966364073dfac3ed92f557b9184e007451b544
rach-vp <picanco.raquel@gmail.com>
2020-11-25 16:16:30 -0400

    Resolução do exercício 5

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.1_/4.1_Javascript_First_Steps/4.1_JS_First_Steps/5.js
@@ -0,0 +1,12 @@
+// Retorna se os três valores de ângulos determinados podem ser ângulos de um triângulo
+
+a = 30;
+b = 90;
+c = 45;
+
+if (a + b + c === 180 ) {
+    console.log('Os ângulos podem ser de um triângulo.');
+}
+else {
+    console.log('Os ângulos não podem ser de um triângulo.');
+}
