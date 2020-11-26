12e00ad49ed3dffae8feaf32f1120ac13d5c07bc
rach-vp <picanco.raquel@gmail.com>
2020-11-26 14:56:06 -0400

    Resolução do exercício 2

--- 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/1.js
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/1.js
@@ -3,7 +3,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i in numbers) {
-  console.log(`O valor da posição ${i} é ${numbers[i]}`)
+  console.log(`O valor da posição ${i} é ${numbers[i]}`);
 }
 
 // Lembrar que no usa da template string o conteúdo deve estar entre crazes (`) e não aspas simples (')
