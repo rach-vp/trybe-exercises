037b72d42bcc60236cc5208581958ca72dab4234
rach-vp <picanco.raquel@gmail.com>
2020-11-26 16:44:18 -0400

    Resolução do exercício 9 e 1 dos bônus

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/Bonus/1.js
@@ -0,0 +1,15 @@
+// Ordena o array numbers utilizando o método bubble sort e imprime o array ordenado
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 1; i < numbers.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (numbers[i] < numbers[j]) {
+      let position = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = position;
+    }
+  }
+}
+
+console.log(`Array em ordem descrescente: ${numbers}`);
