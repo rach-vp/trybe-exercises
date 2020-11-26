a27428701122da361d6061fa65454d02703370ab
rach-vp <picanco.raquel@gmail.com>
2020-11-26 17:04:35 -0400

    Resolução do exercício 3 dos bônus

--- 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/Bonus/2.js
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/Bonus/2.js
@@ -1,15 +1,14 @@
-// Ordena o array numbers em ordem descrescente utilizando o método bubble sort e imprime o array ordenado
+// Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplied = [];
 
-for (let i = 1; i < numbers.length; i++) {
-  for (let j = 0; j < i; j++) {
-    if (numbers[i] > numbers[j]) {
-      let position = numbers[i];
-      numbers[i] = numbers[j];
-      numbers[j] = position;
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if (index === numbers.length - 1) {
+    multiplied.push(numbers[index] * 2);
+  } else {
+    multiplied.push(numbers[index] * numbers[index + 1]);
   }
 }
 
-console.log(`Array em ordem descrescente: ${numbers}`);
+console.log(`Array multiplicado: ${multiplied}`);
