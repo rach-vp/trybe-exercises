dd387ccedfb5c2c376e116c9eea5eb64cc728285
rach-vp <picanco.raquel@gmail.com>
2020-11-26 16:07:58 -0400

    Separa exercícios comuns dos bônus e resolve o 4

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/Exercises/4.js
@@ -0,0 +1,14 @@
+// Imprime a média aritmética dos elementos do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+
+for (let i in numbers) {
+  media += numbers[i];
+}
+media = media / numbers.length
+if (media > 20) {
+  console.log(`O valor da média aritmética dos elementos do array é ${media} e é maior que 20`);
+} else {
+  console.log(`O valor da média aritmética dos elementos do array é ${media} e é manor ou igual a 20`);
+}
