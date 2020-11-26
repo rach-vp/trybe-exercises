d2c0fac9ee6e5294a276d96510d2825645f115aa
rach-vp <picanco.raquel@gmail.com>
2020-11-26 14:48:30 -0400

    Adiciona README.md e resolução do exercício 1

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.2_Array_and_For_Loop/1.js
@@ -0,0 +1,9 @@
+// Varrer o array imprimindo seu conteúdo
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i in numbers) {
+  console.log(`O valor da posição ${i} é ${numbers[i]}`)
+}
+
+// Lembrar que no usa da template string o conteúdo deve estar entre crazes (`) e não aspas simples (')
