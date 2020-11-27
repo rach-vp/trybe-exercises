4c4804cc276f19db3871d7ed9eae0e721a3c6758
rach-vp <picanco.raquel@gmail.com>
2020-11-27 15:52:51 -0400

    Reoslução do exercíico 2

---
+++ 01_Web_Development_Introduction/04_Javascript_Introduction/4.3_Programming_Logic/2.js
@@ -0,0 +1,29 @@
+/*
+
+2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+Copiar
+n = 5
+
+*
+**
+***
+****
+*****
+
+*/
+
+let n = 5;
+
+if (n <=1 ) {
+  console.log('Número inválido.');
+} else {
+  for (let i = 1; i <= n; i += 1) {
+    for (let j = 1; j <= i; j += 1) {
+      if (j !== i) {
+        process.stdout.write('*')
+      } else {
+        console.log('*')
+      }
+    }
+  }
+}
