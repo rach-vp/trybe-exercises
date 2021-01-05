3f0a90f03f88b89849dcb1068d24350bab0cb8a6
rach-vp <picanco.raquel@gmail.com>
2021-01-04 22:09:14 -0400

    Create personal section of CV

--- 01_Intro_Web_Development/06_HTML_and_CSS_Forms/02_Exercises/script.js
+++ 01_Intro_Web_Development/06_HTML_and_CSS_Forms/02_Exercises/script.js
@@ -1,16 +1,51 @@
-// DOM Getters
-const userName = document.querySelector('#full-name');
-const userEmail = document.querySelector('#user-email');
-const userCPF = document.querySelector('#user-cpf');
-const userAddres = document.querySelector('#user-address');
-const userCity = document.querySelector('#user-city');
-const userState = document.querySelector('#user-state');
+function createFullAddress() {
+  const addressSections = document.querySelector('#address-container');
+  const userAddress = document.querySelector('#user-address');
+  const userCity = document.querySelector('#user-city');
+  const userState = document.querySelector('#user-state');
+  const fullAddress = document.createElement('p');
+  fullAddress.innerText = `${userAddress.value}\n${userCity.value}/${userState.value}`;
+  addressSections.appendChild(fullAddress);
+}
+
+function createOtherInfoSection() {
+  const otherInfo = document.querySelector('#other-info-container');
+  const userEmail = document.querySelector('#user-email');
+  const userCPF = document.querySelector('#user-cpf');
+  const email = document.createElement('p');
+  const CPF = document.createElement('p');
+  email.innerText = userEmail.value;
+  CPF.innerText = userCPF.value;
+  otherInfo.appendChild(email);
+  otherInfo.appendChild(CPF);
+}
+
+function createNewHeader() {
+  const userName = document.querySelector('#full-name');
+  const nameHeader = document.createElement('h2');
+  const cvHeader = document.querySelector('#cv-header');
+  nameHeader.innerText = userName.value;
+  cvHeader.appendChild(nameHeader);
+}
+
+function createPersonalSection() {
+  createNewHeader();
+  createOtherInfoSection();
+  createFullAddress();
+}
+
+function createJobSection() {
+
+}
+
+// Event Listeners
 const clearButton = document.querySelector('#clear-btn');
 const submitButton = document.querySelector('#submit-btn');
 
-// Event Listeners
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
+  createPersonalSection();
+  createJobScetion();
 });
 clearButton.addEventListener('click', () => {
   const inputs = document.querySelectorAll('input');
