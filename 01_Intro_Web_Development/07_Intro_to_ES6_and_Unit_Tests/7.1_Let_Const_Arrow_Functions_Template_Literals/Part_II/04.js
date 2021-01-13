6b987bea4995d235d48dccd684919431f1f70d07
rach-vp <picanco.raquel@gmail.com>
2021-01-12 20:15:33 -0400

    Solve exercise 4 final part

--- 01_Intro_Web_Development/07_Intro_to_ES6_and_Unit_Tests/7.1_Let_Const_Arrow_Functions_Template_Literals/Part_II/04.js
+++ 01_Intro_Web_Development/07_Intro_to_ES6_and_Unit_Tests/7.1_Let_Const_Arrow_Functions_Template_Literals/Part_II/04.js
@@ -1,6 +1,21 @@
-const phrase = 'Trybe x aqui!';
-const word = 'oie'
+// Function 1
+const phrase = 'Tryber x here!';
+const word = 'Raquel'
 
-const searchX = (phrase, word) => phrase.replace('x', word);
+const replaceX = (phrase, word) => phrase.replace('x', word);
 
-console.log(`Replacing the word x for "${word}" in phrase "${phrase}" results in ${searchX(phrase, word)}`);
+console.log(`Replacing the word x for "${word}" in phrase "${phrase}" results in "${replaceX(phrase, word)}"`);
+
+//Function 2
+const skills = ['CSS', 'JavaScript', 'Python', 'Ruby', 'Graphic Analysis'];
+
+const catSkills = (phrase, skillsArray) => {
+  skillsArray.sort();
+  finalString = `${phrase} My main skills are:\n`;
+  for (let index = 0; index < skillsArray.length; index += 1) {
+    finalString += `  - ${skillsArray[index]}\n`
+  }
+  return finalString;
+};
+
+console.log(catSkills(replaceX(phrase, word), skills));
