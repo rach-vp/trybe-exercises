3145a76b8898df932761ddf8daf44a48642e4ede
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-23 23:21:31 -0400

    Implements convertion to upper case at name field

--- 02_Front-End/12_Components_with_Status_Events_and_Forms/12.2_forms_in_react/src/components/FormInput/FormInput.jsx
+++ 02_Front-End/12_Components_with_Status_Events_and_Forms/12.2_forms_in_react/src/components/FormInput/FormInput.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 
 export default class FormInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+
+  handleName(input) {
+    this.setState({ name: input.toUpperCase() });
+  }
+
+  handleInput(e) {
+    const input = e.target.value;
+    switch (this.fieldName) {
+      case 'name':
+        return this.handleName(input);
+      default:
+      this.setState((prevState, _props) => ({ [this.type]: input }));
+    }
+  }
+
   render() {
     const { info } = this.props;
     const { fieldName, type, maxLength, required } = info;
+    this.fieldName = fieldName;
 
     return (
       <form>
         <label htmlFor={fieldName}>{`${fieldName}:`}</label>
-        <input type={type} id={fieldName} maxLength={maxLength} required={required} />
+        <input
+          type={type}
+          id={fieldName}
+          maxLength={maxLength}
+          required={required}
+          onChange={this.handleInput.bind(this)}
+          value={
+            Object.keys(this.state).includes(fieldName)
+            ? this.state[fieldName]
+            : ''
+          }
+        />
       </form>
     )
   }
