6f5fcf2470a5290f34975984c359e22fcd911de8
Raquel Picanço <picanco.raquel@gmail.com>
2021-03-01 19:18:59 -0400

    Solve bonus exercise

--- 02_Front-End/13_Component_Life_Cycle_and_React_Router/13.1_life_cycle/src/App.js
+++ 02_Front-End/13_Component_Life_Cycle_and_React_Router/13.1_life_cycle/src/App.js
@@ -8,8 +8,13 @@ class App extends Component {
        loading: true,
        url: '',
        terrier: false,
+       dogName: '',
+       savedNames: [],
     }
     this.fetchDog = this.fetchDog.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.save = this.save.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   fetchDog() {
@@ -25,7 +30,6 @@ class App extends Component {
             } else {
               this.setState({ url: message, loading: false });
             }
-            localStorage.setItem('url', message);
             const breed = message.split('/')[4];
             alert(breed.replace('-', ' '));
           }
@@ -34,12 +38,37 @@ class App extends Component {
       .catch(({ message }) => console.log(message));
   }
 
+  handleChange(e) {
+    this.setState({ dogName: e.target.value })
+  }
+
+  save() {
+    const { dogName, url } = this.state;
+    this.setState(({ savedNames }) => (
+      { savedNames: [...savedNames, { dogName, url }], dogName: '' }
+    ),
+      () => {
+        const { savedNames } = this.state;
+        localStorage.setItem('savedNames', JSON.stringify(savedNames));
+      }
+    )
+  }
+
+  clear() { localStorage.clear(); }
+
   componentDidMount() {
-    this.fetchDog();
+    if (localStorage.length) {
+      const savedNames = JSON.parse(localStorage.getItem('savedNames'));
+      const lastDogURL = Object.values(savedNames[savedNames.length - 1])[1];
+      const lastDogName = Object.values(savedNames[savedNames.length - 1])[0];
+      this.setState({ url: lastDogURL, loading: false, dogName: lastDogName });
+    } else {
+      this.fetchDog();
+    }
   }
 
   render() {
-    const { loading, url, terrier } = this.state;
+    const { loading, url, terrier, dogName } = this.state;
     const loadingComponent = <div>loading...</div>
     const noTerriersComponent = <div>Uh oh! Terriers are forbidden D=</div>
 
@@ -52,7 +81,18 @@ class App extends Component {
           ? loadingComponent
           : terrier
             ? noTerriersComponent
-            : <img src={ url } alt="Some cute doggo <3" style={{ width: "500px" }} />
+            : (
+                <div>
+                  <label>
+                    Give it a name:
+                    <input type="text" onChange= { this.handleChange } value={ dogName }/>
+                  </label>
+                  <button onClick={ this.save }>Save</button>
+                  <button onClick={ this.clear }>Clear storage</button>
+                  <br />
+                  <img src={ url } alt="Some cute doggo <3" style={{ width: "500px" }} />
+                </div>
+              )
         }
       </div>
     );
